4cc2e2c88394883d66691cad08cfb64b51970395
WalterIT <wshiruba@gmail.com>
2024-05-14 19:27:23 -0400

    HEADER COMPONENT: Created DashboardLayout; Header & HeaderLogo Components

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
 
+
 export default function Home() {
   return (
     <>
     <UserButton afterSignOutUrl="/" />
-    <p>This is an Authenticated Route!</p>
+    <p>Dashboard Page!</p>
     </>
   );
 }
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -4,28 +4,24 @@ import { Hono } from 'hono'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { handle } from 'hono/vercel'
 
-import authors from './authors'
-import books from './books'
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
 
-app.route('/authors', authors)
-app.route('/books', books)
 
-// app.get(
-//     '/hello',
-//     clerkMiddleware(),
-//     (c) => {
-//         const auth = getAuth(c);
-//         if(!auth?.userId) return c.json({ error: 'Unauthorized' });
+app.get(
+    '/hello',
+    clerkMiddleware(),
+    (c) => {
+        const auth = getAuth(c);
+        if(!auth?.userId) return c.json({ error: 'Unauthorized' });
 
-//         return c.json({
-//             message: 'Hello Next.js!',
-//             userId: auth?.userId
-//         })
-//     })
+        return c.json({
+            message: 'Hello Next.js!',
+            userId: auth?.userId
+        })
+    })
 
 
 
