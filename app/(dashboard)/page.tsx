fb528949b3cc0edcc4198d6e4e1c1bc783ce8724
WalterIT <wshiruba@gmail.com>
2024-05-14 10:23:01 -0400

    IMPLEMENTED AUTHENTICATION with CLERK

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
+    <>
+    <UserButton afterSignOutUrl="/" />
     <p>This is an Authenticated Route!</p>
+    </>
   );
 }
